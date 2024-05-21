2bd2155f6e9510e7b0b54c3df231084cb71af2d3
Xuan Son <doxuanson12b2003@gmail.com>
2024-05-21 20:23:48 +0700

    feat: Complete ProductDetail component UI

--- src/Pages/ProductDetail.jsx
+++ src/Pages/ProductDetail.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+
+import './SCSS/ProductDetail.scss';
 import all_product from '~/Assets/Images/all_product';
 import Breadcrum from '~/Components/BreadScrum/Breadcrum';
 import DescriptionBox from '~/Components/DescriptionBox/DescriptionBox';
+import Popular from '~/Components/Popular/Popular';
 import ProductDisplay from '~/Components/ProductDisplay/ProductDisplay';
 
 const ProductDetail = () => {
@@ -14,6 +17,7 @@ const ProductDetail = () => {
                 <Breadcrum product={product} />
                 <ProductDisplay product={product} />
                 <DescriptionBox />
+                <Popular title="Thời trang nam phổ biến" category="men" />
             </div>
         </div>
     );
