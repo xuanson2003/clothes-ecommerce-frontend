4c753e87566dfcabfa21af9c191d7c59bb1cd1f5
Xuan Son <doxuanson12b2003@gmail.com>
2024-05-24 11:43:50 +0700

    feat: optimize logic and add validation for Login component, complete logic and validation for SignUp component

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,19 +1,9 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext } from 'react';
 
 const ShopContext = createContext(null);
 
-const getDefaultCart = () => {
-    let cart = {};
-    for (let index = 0; index < 300 + 1; index++) {
-        cart[index] = 0;
-    }
-    return cart;
-};
-
 const ShopContextProvider = ({ children }) => {
-    const [cartItems, setCartItems] = useState(getDefaultCart());
-
-    return <ShopContext.Provider>{children}</ShopContext.Provider>;
+    return <ShopContext.Provider value={null}>{children}</ShopContext.Provider>;
 };
 
 export { ShopContext, ShopContextProvider };
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 import config from '~/Config';
 import './SCSS/LoginSignup.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,106 +11,104 @@ import request from '~/Utils/httpRequest';
 
 const Login = () => {
     const [passwordVisible, setPasswordVisible] = useState(false);
-    const [passwordError, setPasswordError] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [loginError, setLoginError] = useState('');
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    });
 
-    function handleChange(e) {
-        setEmailError('');
-        setPasswordError('');
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    }
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().email('Email không hợp lệ').required('Email là bắt buộc'),
+            password: Yup.string().required('Mật khẩu là bắt buộc'),
+        }),
+        onSubmit: async (values, { setSubmitting, setErrors }) => {
+            try {
+                const response = await request.post('login', values);
 
-    async function handleLogin() {
-        try {
-            const response = await request({
-                method: 'post',
-                url: 'login',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                data: formData,
-            });
+                const responseData = response.data;
 
-            const responseData = response.data;
-
-            if (responseData.success) {
-                localStorage.setItem('auth-token', responseData.token);
-                window.location.replace('/');
-            } else {
-                if (responseData.errorField === 'email') {
-                    setEmailError('Email này chưa được đăng ký');
-                } else if (responseData.errorField === 'password') {
-                    setPasswordError('Mật khẩu không chính xác');
+                if (responseData.success) {
+                    localStorage.setItem('auth-token', responseData.token);
+                    // window.location.replace('/');
+                } else {
+                    if (responseData.errorField === 'email') {
+                        setErrors({ email: 'Email này chưa được đăng ký' });
+                    } else if (responseData.errorField === 'password') {
+                        setErrors({ password: 'Mật khẩu không chính xác' });
+                    }
                 }
+            } catch (error) {
+                setErrors({ submit: 'Đăng nhập thất bại. Vui lòng thử lại sau.' });
+            } finally {
+                setSubmitting(false);
             }
-        } catch (error) {
-            setLoginError('Đăng nhập thất bại. Vui lòng thử lại sau.');
-        }
-    }
+        },
+    });
 
     return (
         <div className="login">
-            <div className="login-form">
-                <h2>Đăng nhập</h2>
-                <h3>{loginError}</h3>
-                <div className="login-form-fields">
-                    <div className="login-form-field">
-                        <input
-                            className={emailError ? 'error' : ''}
-                            name="email"
-                            onChange={handleChange}
-                            type="email"
-                            placeholder="Email"
-                        />
-                        <p className="login-form-field-error">{emailError}</p>
-                    </div>
-                    <div className="login-form-field">
-                        <div className="login-form-field-group">
+            <div className="login-container">
+                <form className="login-form" onSubmit={formik.handleSubmit}>
+                    <h2>Đăng nhập</h2>
+                    {formik.errors.submit && <h3>{formik.errors.submit}</h3>}
+                    <div className="login-form-fields">
+                        <div className="login-form-field">
                             <input
-                                className={passwordError ? 'error' : ''}
-                                name="password"
-                                onChange={handleChange}
-                                type={passwordVisible ? 'text' : 'password'}
-                                placeholder="Mật khẩu"
+                                className={formik.touched.email && formik.errors.email ? 'error' : ''}
+                                name="email"
+                                type="email"
+                                placeholder="Email"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.email}
                             />
-                            {passwordVisible ? (
-                                <FontAwesomeIcon
-                                    onClick={() => {
-                                        setPasswordVisible(false);
-                                    }}
-                                    className="login-form-field-icon"
-                                    icon={faEye}
-                                />
-                            ) : (
-                                <FontAwesomeIcon
-                                    onClick={() => {
-                                        setPasswordVisible(true);
-                                    }}
-                                    className="login-form-field-icon"
-                                    icon={faEyeSlash}
-                                />
-                            )}
+                            {formik.touched.email && formik.errors.email ? (
+                                <p className="login-form-field-error">{formik.errors.email}</p>
+                            ) : null}
                         </div>
-                        <p className="login-form-field-error">{passwordError}</p>
+                        <div className="login-form-field">
+                            <div className="login-form-field-group">
+                                <input
+                                    className={formik.touched.password && formik.errors.password ? 'error' : ''}
+                                    name="password"
+                                    type={passwordVisible ? 'text' : 'password'}
+                                    placeholder="Mật khẩu"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.password}
+                                />
+                                {passwordVisible ? (
+                                    <FontAwesomeIcon
+                                        onClick={() => setPasswordVisible(false)}
+                                        className="login-form-field-icon"
+                                        icon={faEye}
+                                    />
+                                ) : (
+                                    <FontAwesomeIcon
+                                        onClick={() => setPasswordVisible(true)}
+                                        className="login-form-field-icon"
+                                        icon={faEyeSlash}
+                                    />
+                                )}
+                            </div>
+                            {formik.touched.password && formik.errors.password ? (
+                                <p className="login-form-field-error">{formik.errors.password}</p>
+                            ) : null}
+                        </div>
                     </div>
-                </div>
-                <div className="login-form-forgot-password">
-                    <a href="/forgot-password">Bạn quên mật khẩu?</a>
-                </div>
-                <button onClick={handleLogin} className="login-form-button">
-                    Đăng nhập
-                </button>
-                <div className="login-form-register-link">
-                    <span>
-                        Bạn chưa có tài khoản? <Link to={config.routes.signup}>Đăng ký</Link>
-                    </span>
-                </div>
+                    <div className="login-form-forgot-password">
+                        <a href="/forgot-password">Bạn quên mật khẩu?</a>
+                    </div>
+                    <button type="submit" className="login-form-button" disabled={formik.isSubmitting}>
+                        Đăng nhập
+                    </button>
+                    <div className="login-form-register-link">
+                        <span>
+                            Bạn chưa có tài khoản? <Link to={config.routes.signup}>Đăng ký</Link>
+                        </span>
+                    </div>
+                </form>
+
                 <div className="login-form-or-separator">
                     <hr />
                     <span>hoặc</span>
